2c0aee09b914b77ba2b038427416c9a908ccd7ce
muradil <04072002mu@gmail.com>
2022-10-19 00:17:38 +0600

    init types auth

--- src/store/reducers/auth/index.ts
+++ src/store/reducers/auth/index.ts
@@ -1,11 +1,14 @@
+import {AuthAction, AuthActionEnum, AuthState} from "./types";
 
-const initialState = {
+const initialState: AuthState = {
   auth: false
 }
 
-export default function authReducer(state, action){
+export default function authReducer(state = initialState, action: AuthAction): AuthState{
   switch (action.type) {
-    default:
+      case AuthActionEnum.SET_AUTH:
+          return {...state, auth: action.payload}
+      default:
       return state;
   }
 }
